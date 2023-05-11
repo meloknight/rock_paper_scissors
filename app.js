20e5cb9678913a081bf09d978f63c3f55f72943f
Cornelius <49502879+meloknight@users.noreply.github.com>
2023-05-10 17:27:15 -0700

    update rock_paper_scissors game to have a rough UI and refactor the logic to use the DOM as opposed to prompts.

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 
-game();
+const rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
+const roundNumberDisplay = document.querySelector('#round-number-display');
+const winsDisplay = document.querySelector('#wins-display');
+const lossesDisplay = document.querySelector('#losses-display');
+const drawsDisplay = document.querySelector('#draws-display');
+const playerChoiceContainer = document.querySelector('#player-choice');
+const computerChoiceContainer = document.querySelector('#computer-choice');
+const outcomeContainer = document.querySelector('#outcome-container');
 
+const choiceButtons = document.querySelectorAll('.choose-button');
+// console.log(choiceButtons);
+
+game();
 
 function getComputerChoice() {
     let randNum = 3 * Math.random();
@@ -24,16 +37,16 @@ function getComputerChoice() {
     return computerChoice;
 };
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Choose rock, paper, or scissors!");
-    playerChoice = playerChoice.toLowerCase();
+function getPlayerChoice(choiceId) {
+    // let playerChoice = prompt("Choose rock, paper, or scissors!");
+    // playerChoice = playerChoice.toLowerCase();
     let finalPlayerChoice;
     
-    if (playerChoice.toLowerCase() === "rock") {
+    if (choiceId === "rock-button") {
         finalPlayerChoice = "rock";
-    } else if (playerChoice.toLowerCase() === "paper") {
+    } else if (choiceId === "paper-button") {
         finalPlayerChoice = "paper";
-    } else if (playerChoice.toLowerCase() === "scissors") {
+    } else if (choiceId === "scissors-button") {
         finalPlayerChoice = "scissors";
     } else {
         finalPlayerChoice = "Improper Selection!!"
@@ -88,24 +101,55 @@ function playRound(computerSelection, playerSelection, wins, losses, draws) {
 
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+    // let playerScore = 0;
+    // let computerScore = 0;
     let wins = 0;
     let losses = 0;
     let draws = 0;
+    let i = 0;
     
-    for (let i = 0; i < 5; i++) {
-        console.log(`This is round ${i+1}`);
-        let compChoice = getComputerChoice();
-        let playChoice = getPlayerChoice(); 
-        let roundResult = playRound(compChoice, playChoice, wins, losses, draws);
-        wins = roundResult[1];
-        losses = roundResult[2];
-        draws = roundResult[3];
+    // for (let i = 0; i < 10; i++) {
+    choiceButtons.forEach(element => {
+        element.addEventListener('click', function(e) {
+            // console.log(`This is round ${i+1}`);
+            i++;
+            roundNumberDisplay.textContent = `Round Number: ${i+1}`;
+    
+            // console.log(e.target.id);
 
-        console.log(`player has chosen: ${playChoice}`);
-        console.log(`computer has chosen: ${compChoice}`);
-        console.log(roundResult);
-        console.log(`wins = ${wins} : losses = ${losses} : draws = ${draws}`);
-    }
+            let compChoice = getComputerChoice();
+            let playChoice = getPlayerChoice(e.target.id); 
+            // let playChoice = 'rock';
+            let roundResult = playRound(compChoice, playChoice, wins, losses, draws);
+            wins = roundResult[1];
+            losses = roundResult[2];
+            draws = roundResult[3];
+            
+            playerChoiceContainer.textContent = `Player Choice: ${playChoice}`
+            computerChoiceContainer.textContent = `Computer Choice: ${compChoice}`
+            winsDisplay.textContent = `Wins: ${wins}`;
+            lossesDisplay.textContent = `Losses: ${losses}`;
+            drawsDisplay.textContent = `Draws: ${draws}`;
+            outcomeContainer.textContent = roundResult[0];
+    
+            // console.log(`player has chosen: ${playChoice}`);
+            // console.log(`computer has chosen: ${compChoice}`);
+            // console.log(roundResult);
+            // console.log(`wins = ${wins} : losses = ${losses} : draws = ${draws}`);
+
+            if (wins >= 5) {
+                window.alert("You won 5 rounds! You're a winner!")
+            } else if (losses >= 5) {
+                window.alert("You lost 5 rounds! You're a loser!")
+            }
+
+        })
+    })
+
+
+    // }
 };
+
+
+
+
